0f5fa4b20b08e51bc6d4f0de3da2831582a7928d
Magno Moreira <magnomoreira@yaho.com>
2021-07-06 18:52:25 -0300

    Creating Project

---
+++ PersonApi/PersonApi/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonApi.Controllers
+{
+	public class PersonController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
