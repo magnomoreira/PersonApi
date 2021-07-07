d8144b2fec4e6b93f99795c0247cdf4bb6479b57
Magno Moreira <magnomoreira@yaho.com>
2021-07-06 23:02:12 -0300

    Implementing Controller

--- PersonApi/PersonApi/Controllers/PersonController.cs
+++ PersonApi/PersonApi/Controllers/PersonController.cs
@@ -1,12 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PersonApi.Model;
+using PersonApi.Services.Implementations;
 
 namespace PersonApi.Controllers
 {
-	public class PersonController : Controller
+	public class PersonController : ControllerBase
 	{
-		public IActionResult Index()
+		private readonly ILogger<PersonController> _logger;
+		private IPersonService _personService;
+		public PersonController(ILogger<PersonController> logger, IPersonService personService)
 		{
-			return View();
+			_logger = logger;
+			_personService = personService;
 		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(_personService.FindAll());
+
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(long id)
+		{
+			var person = _personService.FindById(id);
+			if (person == null) return NotFound();
+
+			return Ok(person);
+
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] Person person)
+		{
+			if (person == null) return BadRequest();
+
+			return Ok(_personService.Create(person));
+
+		}
+
+
+		[HttpPut]
+		public IActionResult Put([FromBody] Person person)
+		{
+			if (person == null) return BadRequest();
+
+			return Ok(_personService.Update(person));
+
+		}
+
+
+		[HttpDelete("{id}")]
+		public IActionResult Delite(long id)
+		{
+			_personService.Delete(id);
+
+			return NoContent();
+
+		}
+
+
 	}
 }
--- PersonApi/PersonApi/Startup.cs
+++ PersonApi/PersonApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PersonApi.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,9 @@ namespace PersonApi
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			// Adicionando injeção de dependecia
+			services.AddScoped<IPersonService, PersonServiceImplementation>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
